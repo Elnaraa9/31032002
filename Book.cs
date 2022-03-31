4f44de4b4f021bb5998b4106fbcc7eca3787e479
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-03-31 19:32:58 +0400

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace _31032002.AccessModifiers
+{
+    class Book:Product
+    {
+        public string Genre;
+        public bool Cretaed;
+        public Book(string genre, int no, string name, double price, int count): base(no, name, price, count)
+        {
+            Genre = genre;
+            if (genre==""||genre==" ")
+            {
+                Console.WriteLine("Bele bir janr movcud deyil");
+            }
+            Cretaed = true;
+        }
+    }
+}
